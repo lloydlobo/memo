47968f6944c5e8f7a7290c431172fc640794dc1d
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-16 16:28:45 +0530

    feat(utils): add utility getError func toast error

---
+++ utils/getError.ts
@@ -0,0 +1,22 @@
+/**
+ * toastError.
+ */
+export type toastError =
+  | { response: { data: { message: any } }; message: any }
+  | any
+  | unknown;
+
+/**
+ *  getError().
+ *
+ *  Optional chaining:
+ *  (err.response && err.response.data && err.response.data.message)
+ */
+// https://youtu.be/fBew9hQhvLU?t=551
+export function getError({ err }: { err: toastError }): toastError["message"] {
+  if (err.response?.data?.message) {
+    return err.response.data.message;
+  }
+
+  return err.message;
+}
