f736e9b2de35e6da17fb014b0fb78a36a82982b6
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-16 15:05:35 +0530

    feat(componenets): add toast daisy ui component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,34 +1,54 @@
 import Head from "next/head";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Navbar from "./navbar/Navbar";
 import { NavbarBottom } from "./navbar/NavbarBottom";
+import { ToastTopStart } from "./toast/ToastTopStart";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div className="wrapper ">
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+function Layout({ children, title = "This is the default title" }: Props) {
+  const [loading, setLoading] = useState(true);
 
-    <header className="bg-base-300 shadow-md">
-      <Navbar />
-    </header>
+  // Preloader mock.
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(!loading);
+    }, 1000);
+  }, []);
 
-    {children}
-    <div className="sm:hidden">
-      <NavbarBottom />
+  // JSX.Element.
+  return (
+    <div className="wrapper ">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <header className="bg-base-300 shadow-md">
+        <Navbar />
+      </header>
+
+      {loading ? (
+        <ToastTopStart alert1={"Loading..."} alert2={""} />
+      ) : (
+        <>{children}</>
+      )}
+
+      <footer>
+        <div className="sm:hidden">
+          <NavbarBottom />
+        </div>
+        <div className="hidden sm:grid">
+          <hr />
+          <span>I'm here to stay (Footer)</span>
+        </div>
+      </footer>
     </div>
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-);
+  );
+}
 
 export default Layout;
---
+++ components/toast/ToastTopStart.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function ToastTopStart(props: { alert1: any; alert2: any }) {
+  let { alert1, alert2 } = props;
+  return (
+    <div className="toast-start toast toast-top py-24">
+      {alert1 !== "" ? (
+        <div className="alert alert-info">
+          <div>
+            <span>{alert1}</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      {alert2 !== "" ? (
+        <div className="alert alert-success">
+          <div>
+            <span>{alert2}</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
