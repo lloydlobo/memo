d1baf531fa8349050065a47d1775a3e3e92e0ede
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-18 05:07:45 +0530

    style(components): fix no dropdown bg color menu

--- components/navbar/Menu.tsx
+++ components/navbar/Menu.tsx
@@ -64,7 +64,7 @@ export default function MenuDropdown({ children }: any): JSX.Element {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="absolute  right-0 mt-2 w-56 origin-top-right divide-y divide-secondary divide-opacity-60 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-secondary divide-opacity-60 rounded-md bg-base-300 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="px-1 py-1 ">
                             <Menu.Item>
                                 {({ active }) => (
