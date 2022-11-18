27d23e0028f4d79b6c8f4793a9fe60160832989d
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-18 06:15:10 +0530

    fix(pages): add title for Layout meta head

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,9 +3,9 @@ import { LoginForm } from "../components/auth/LoginForm";
 import Layout from "../components/Layout";
 
 export default function login() {
-  return (
-    <Layout>
-      <LoginForm />
-    </Layout>
-  );
+    return (
+        <Layout title="Login Page">
+            <LoginForm />
+        </Layout>
+    );
 }
