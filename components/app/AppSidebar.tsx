a97e922c0767ebb2a5871928c315612da37356d7
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-19 18:10:39 +0530

    feat(components): add disclosure sidebar buttons

--- components/app/AppSidebar.tsx
+++ components/app/AppSidebar.tsx
@@ -1,4 +1,7 @@
-import { HamburgerIcon } from "../icons";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { HamburgerIcon, InboxIcon } from "../icons";
 
 export default function AppSidebar({
     toggleDrawer,
@@ -19,14 +22,106 @@ export default function AppSidebar({
                     <HamburgerIcon className="h-auto w-5" />
                 </button>
                 <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                <ul className="menu w-auto p-4 text-base-content">
+                <ul className="menu menu-compact w-auto pt-5 text-base-content">
                     {/*       <!-- Sidebar content here --> */}
                     <li>
-                        <a>Sidebar Item 1</a>
+                        <Link href="/app">
+                            <InboxIcon className="h-5" />
+                            <span>Inbox</span>
+                        </Link>
                     </li>
                     <li>
-                        <a>Sidebar Item 2</a>
+                        <Link href="/app">
+                            <InboxIcon className="h-5" />
+                            <span>Today</span>
+                        </Link>
                     </li>
+                    <li>
+                        <Link href="/app">
+                            <InboxIcon className="h-5" />
+                            <span>Upcoming</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/app">
+                            <InboxIcon className="h-5" />
+                            <span>Filters & Labels</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/app">
+                            <InboxIcon className="h-5" />
+                            <span>Completed</span>
+                        </Link>
+                    </li>
+                    <Disclosure>
+                        {({ open }) => (
+                            <>
+                                <li className="mt-5">
+                                    <Disclosure.Button className="flex w-full py-2 font-semibold text-base-content/70">
+                                        <span className="mr-auto">
+                                            Favorites
+                                        </span>
+                                        <ChevronUpIcon
+                                            className={` ${
+                                                open
+                                                    ? "-rotate-180 transform"
+                                                    : "-rotate-90 transform"
+                                            } h-5 w-5`}
+                                        />
+                                    </Disclosure.Button>
+                                </li>
+                                <Disclosure.Panel className="">
+                                    <li>
+                                        <Link href="">
+                                            <InboxIcon className="h-5" />
+                                            <span>This Week</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="">
+                                            <InboxIcon className="h-5" />
+                                            <span>View all</span>
+                                        </Link>
+                                    </li>
+                                </Disclosure.Panel>
+                            </>
+                        )}
+                    </Disclosure>
+                    <Disclosure>
+                        {({ open }) => (
+                            <>
+                                <li className="mt-5">
+                                    <Disclosure.Button className="flex w-full py-2 font-semibold text-base-content/70">
+                                        <span className="mr-auto">
+                                            Projects
+                                        </span>
+                                        <ChevronUpIcon
+                                            className={` ${
+                                                open
+                                                    ? "-rotate-180 transform"
+                                                    : "-rotate-90 transform"
+                                            } h-5 w-5`}
+                                        />
+                                    </Disclosure.Button>
+                                </li>
+                                <Disclosure.Panel className="">
+                                    <li>
+                                        <Link href="">
+                                            <InboxIcon className="h-5" />
+                                            <span>Project 1</span>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="">
+                                            <InboxIcon className="h-5" />
+                                            <span>Project 2</span>
+                                        </Link>
+                                    </li>
+                                </Disclosure.Panel>
+                            </>
+                        )}
+                    </Disclosure>
                 </ul>
             </div>
         </>
