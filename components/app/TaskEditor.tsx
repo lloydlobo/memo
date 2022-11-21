b9170373e06d0ab91a67a053a608728f6f7fb0da
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-21 16:27:02 +0530

    feat: add editIndex state & fluid textarea

--- components/app/TaskEditor.tsx
+++ components/app/TaskEditor.tsx
@@ -6,16 +6,31 @@ import TaskEditorFooter from "./TaskEditorFooter";
 export function TaskEditor({
     task,
     onCancel,
+    index,
     onTaskHover,
+    className,
+    editIndex,
 }: {
     task: TaskData;
     onCancel: any;
+    index: number;
     onTaskHover: boolean;
+    className: string;
+    editIndex: number | null;
 }) {
     return (
         <>
-            <form action="" className="task-editor">
-                <TaskEditorArea task={task} />
+            <form
+                action=""
+                // className="task-editor"
+                id="taskEditor"
+                className={className}
+            >
+                <TaskEditorArea
+                    index={index}
+                    task={task}
+                    editIndex={editIndex}
+                />
                 <TaskEditorFooter onCancel={onCancel} />
                 {/* <TaskEditorFooter onCancel={() => {}} onSave={() => {}} /> */}
             </form>
